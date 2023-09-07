9eb4aa99f7e23e48635dfff6146b143c545db1c0
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-09-07 15:37:59 +0700

    update sign in google auth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,22 @@
+"use client";
+
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const LoginPage = () => {
+  const { data, status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+  if (status === "authenticated") {
+    router.push("/");
+  }
+
   return (
     <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
       {/* BOX */}
@@ -15,7 +29,10 @@ const LoginPage = () => {
         <div className="p-10 flex flex-col gap-8 md:w-1/2">
           <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
           <p>Log into your account or create a new one using social buttons</p>
-          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+          <button
+            className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md"
+            onClick={() => signIn("google")}
+          >
             <Image
               src="/google.png"
               alt=""
