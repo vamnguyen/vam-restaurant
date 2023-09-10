48b11f6ad5a39eab5f4581568f1dd083ccc90c5f
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-09-10 16:55:11 +0700

    test login with fb

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -42,7 +42,10 @@ const LoginPage = () => {
             />
             <span>Sign in with Google</span>
           </button>
-          <button className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md">
+          <button
+            className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md"
+            onClick={() => signIn("facebook")}
+          >
             <Image
               src="/facebook.png"
               alt=""
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,8 @@
 import { NextAuthOptions, getServerSession, User } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "./connect";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 
 declare module "next-auth" {
   interface Session {
@@ -26,6 +27,10 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
