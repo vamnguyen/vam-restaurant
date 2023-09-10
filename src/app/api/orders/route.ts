61057007d537ad03e07288f67e3ceb80b803a76e
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-09-10 16:14:16 +0700

    create api fetch all orders and user role authorza

---
+++ src/app/api/orders/route.ts
@@ -0,0 +1,42 @@
+import { getAuthSession } from "@/utils/auth";
+import { prisma } from "@/utils/connect";
+import { NextRequest, NextResponse } from "next/server";
+
+// FETCH ALL ORDERS
+export const GET = async (req: NextRequest) => {
+  // get user data signed in to session
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      // User is Admin
+      if (session.user.isAdmin) {
+        const orders = await prisma.order.findMany();
+        return new NextResponse(JSON.stringify(orders), { status: 200 });
+      }
+
+      // User is not Admin
+      const orders = await prisma.order.findMany({
+        where: {
+          userEmail: session.user.email!,
+        },
+      });
+      return new NextResponse(JSON.stringify(orders), { status: 200 });
+    } catch (error) {
+      console.log(error);
+      return new NextResponse(
+        JSON.stringify({ message: "Something went wrong!" }),
+        { status: 500 }
+      );
+    }
+  } else {
+    return new NextResponse(
+      JSON.stringify({ message: "You are not authenticated!" }),
+      { status: 401 }
+    );
+  }
+};
+
+export const POST = () => {
+  return new NextResponse("Hello method POST", { status: 201 });
+};
