b296e5143aeadc1c83e6f8a7825cbed0c5bc513f
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-09-16 08:00:33 +0700

    fix error PUT method in api confirm

--- src/app/api/(checkout)/confirm/[intentId]/route.ts
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -1,7 +1,10 @@
 import { prisma } from "@/utils/connect";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const PUT = async ({ params }: { params: { intentId: string } }) => {
+export const PUT = async (
+  request: NextRequest,
+  { params }: { params: { intentId: string } }
+) => {
   const { intentId } = params;
 
   try {
