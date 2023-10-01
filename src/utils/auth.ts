3861cf06c4cde0fef1833d269d06ec9e5cc4a8a8
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-10-01 14:02:21 +0700

    debug sign in google

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -50,6 +50,7 @@ export const authOptions: NextAuthOptions = {
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
+  debug: true,
 };
 
 export const getAuthSession = () => getServerSession(authOptions);
